62c47664704789cebec5612eebe9d4d252aa960d
SundayC57 <sundaycs@mail.uc.edu>
2026-09-21 18:18:48 -0400

    finished commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 void main() {
 
+int inputPrice = 30;
+double salesTax = 0;
 
+salesTax = 0.05 * inputPrice;
+
+System.out.println("The sales tax on your purchase of $" + inputPrice + " is $" + salesTax);
 
 }
